6a1dd5efce84861ce17c831298fa7e1cf1b3ad60
Gabe Pires <gabe.a.pires@gmail.com>
2015-05-19 22:16:03 -0700

    The core classes
    - helper class NSDictionary+dynamicAds_service
    - UIView to add the server interstitial ad

---
+++ NSDictionary+dynamicAds_service.m
@@ -0,0 +1,47 @@
+//
+//  NSDictionary+dynamicAds_service.m
+//  ServerAdsExample
+//
+//  Created by Pires on 5/19/15.
+//  Copyright (c) 2015 PerpetualApps. All rights reserved.
+//
+
+#import "NSDictionary+dynamicAds_service.h"
+
+@implementation NSDictionary (dynamicAds_service)
+
+#warning depending on json file, change ad_data and such to needs
+- (NSDictionary *)serverAd
+{
+    //everything inside root
+    NSDictionary *dict = self[@"root"];
+    //everything inside iBooks
+    NSArray *ar = dict[@"object1"];
+    return ar[0];
+}
+
+//just showing that you can get other data besides the one in the above function
+- (NSDictionary *)serverAd2
+{
+    NSDictionary *dict = self[@"root"];
+    NSArray *ar = dict[@"object2"];
+    return ar[0];
+}
+
+//get the URL link to the app in the appstore
+- (NSString *)adLinkURL
+{
+    NSArray *ar = self[@"ad_link"];
+    NSDictionary *dict = ar[0];
+    return dict[@"value"];
+}
+
+//get the advertisement image link
+- (NSString *)adImageURL
+{
+    NSArray *ar = self[@"ad_image"];
+    NSDictionary *dict = ar[0];
+    return dict[@"value"];
+}
+
+@end
---
+++ ServerAd.h
@@ -0,0 +1,13 @@
+//
+//  ServerAd.h
+//  ServerAdsExample
+//
+//  Created by Pires on 5/19/15.
+//  Copyright (c) 2015 PerpetualApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ServerAd : UIView
+
+@end
